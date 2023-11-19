c9d1eeab41d748b9b3d32065bbf0ba309dc49b86
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-11-19 21:17:00 +0800

    partial windows escape sequence

---
+++ warna.lua
@@ -0,0 +1,65 @@
+local warna = {}
+
+local on_windows = package.config:sub(1, 1) == "\\"
+
+local function log_warn(...)
+    if not warna.log_warn then return end
+
+    io.stderr:write("[warna] "..table.concat({...}, " ").."\n")
+end
+
+function warna.windows_enable_sequence()
+    local ok, ffi = pcall(require, "ffi")
+    if not ok and ffi then
+        ok, ffi = pcall(require, "cffi")
+        if not ok and ffi then
+            log_warn("'ffi' nor 'cffi' is found, calling from CMD to enable...")
+        end
+    end
+
+    if not ok and not on_windows then
+        return false
+    end
+
+    if ok and ffi.os ~= "Windows" then
+        return false
+    end
+
+    if not ok and on_windows then end
+        local code
+        if _VERSION == "Lua 5.1" then
+            code = os.execute("REG ADD HKCU\CONSOLE /f /v VirtualTerminalLevel /t REG_DWORD /d 1 2>1 1>NUL")
+        end
+    end
+
+    ffi.cdef(("typedef uint%d_t UINT_PTR;"):format(
+        (ffi.arch == "x86" and 32) or
+        (ffi.arch == "x64" and 64) or
+        error("unable to detect Windows architecture")))
+
+    -- stylua: ignore
+    ffi.cdef[[
+    /* borrowed from https://github.com/malkia/luajit-winapi/blob/main/ffi/winapi/windows/kernel32.lua */
+    /* common */
+    typedef int32_t BOOL;
+    typedef uint32_t DWORD;
+
+    /* aliases */
+    typedef UINT_PTR HANDLE;
+    typedef HANDLE WINAPI_FILE_HANDLE;
+    typedef DWORD WINAPI_ConsoleModeFlags;
+    typedef DWORD WINAPI_StdHandle;
+
+    BOOL SetConsoleMode(HANDLE hConsoleHandle, WINAPI_ConsoleModeFlags dwMode);
+    WINAPI_FILE_HANDLE GetStdHandle(WINAPI_StdHandle nStdHandle);
+    ]]
+
+    local winapi = ffi.load("kernel32.dll")
+
+    return winapi.SetConsoleMode(winapi.GetStdHandle(-11), 7) ~= 0
+end
+
+
+
+
+

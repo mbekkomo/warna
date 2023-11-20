6d2c0f0d399af78db5840078038bcf25a7a26aad
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-11-21 00:16:38 +0800

    partial windows escape sequence

--- warna.lua
+++ warna.lua
@@ -2,35 +2,72 @@ local warna = {}
 
 local on_windows = package.config:sub(1, 1) == "\\"
 
-local function log_warn(...)
-    if not warna.log_warn then return end
-
-    io.stderr:write("[warna] "..table.concat({...}, " ").."\n")
+---@param cmd string
+---@return integer
+local function execute_cmd(cmd)
+    local code
+    if _VERSION == "Lua 5.1" and not jit then
+        code = os.execute(cmd)
+    else
+        _, _, code = os.execute(cmd)
+    end
+    return code ---@diagnostic disable-line:return-type-mismatch
 end
 
-function warna.windows_enable_sequence()
+---@param cmd string
+---@param raw boolean?
+---@return string?
+local function cmd_output(cmd, raw)
+    local fd, err = io.popen(cmd, "r")
+    local output
+    if fd and not err then
+        output = fd:read("*a")
+        fd:close()
+    else
+        return
+    end
+    if raw then
+        return output
+    end
+
+    output = output:gsub("^%s+", ""):gsub("%s+$", "")
+
+    return output
+end
+
+--- 
+--- Patch the Windows ANSI sequences problem.
+---
+--- Requires Windows 10 build after 14393 (Anniversary update) and `ffi` or [`cffi`](https://github.com/q66/cffi-lua) to patch.
+--- If not fallbacks to editing registry.
+--- 
+--- For Windows 10 before build 14393 (Anniversary update) or before Windows 10, requires [ANSICON](https://github.com/adoxa/ansicon) to patch.
+---
+---@return boolean
+function warna.windows_enable_sequences()
     local ok, ffi = pcall(require, "ffi")
     if not ok and ffi then
         ok, ffi = pcall(require, "cffi")
-        if not ok and ffi then
-            log_warn("'ffi' nor 'cffi' is found, calling from CMD to enable...")
-        end
     end
 
-    if (not ok and not on_windows) or
-       (ok and ffi.os ~= "Windows") then
+    if not (ok and on_windows) or (ok and ffi.os ~= "Windows") then
         return false
     end
 
+    local _, ver = (cmd_output("ver") or ""):match("(%[Version (.+)%])$")
+    local winver, buildver = ver:match("^(%d+%.%d+)%.(%d+)")
+
+    if ver and (tonumber(winver) >= 10 and not buildver >= "14393") or tonumber(winver) < 10 then
+        return execute_cmd((os.getenv("ANSICON") or "ansicon") .. " -p") == 0
+    end
+
     if not ok and on_windows then
-        local code
-        if _VERSION == "Lua 5.1" then
-            code = os.execute("REG ADD HKCU\\CONSOLE /f /v VirtualTerminalLevel /t REG_DWORD /d 1 2>1 1>NUL")
-        else
-            _, _, code = os.execute("REG ADD HKCU\\CONSOLE /f /v VirtualTerminalLevel /t REG_DWORD /d 1 2>1 1>NUL")
+        if execute_cmd("reg query HKCU\\CONSOLE /v VirtualTerminalLevel 2>1 1>NUL") == 0 then
+            return true
         end
 
-        return code == 0
+        io.write
+        return execute_cmd("reg add HKCU\\CONSOLE /f /v VirtualTerminalLevel /t REG_DWORD /d 1 2>1 1>NUL") == 0
     end
 
     -- stylua: ignore
@@ -38,6 +75,7 @@ function warna.windows_enable_sequence()
     /* borrowed from https://github.com/malkia/luajit-winapi */
 
     typedef uintptr_t UINT_PTR;
+
     typedef int32_t BOOL;
     typedef uint32_t DWORD;
 
@@ -55,6 +93,4 @@ function warna.windows_enable_sequence()
     return winapi.SetConsoleMode(winapi.GetStdHandle(-11), 7) ~= 0
 end
 
-return function()
-    return warna
-end
+return warna

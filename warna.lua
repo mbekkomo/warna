2c6d3806ec65354bb6960665a5c903413d8504b7
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-11-21 21:13:30 +0000

    feat: `windows_enable_sequences`

--- warna.lua
+++ warna.lua
@@ -35,39 +35,43 @@ local function cmd_output(cmd, raw)
     return output
 end
 
---- 
---- Patch the Windows ANSI sequences problem.
+--- Patch the Windows ASGR sequences problem.
 ---
 --- Requires Windows 10 build after 14393 (Anniversary update) and `ffi` or [`cffi`](https://github.com/q66/cffi-lua) to patch.
 --- If not fallbacks to editing registry.
---- 
---- For Windows 10 before build 14393 (Anniversary update) or before Windows 10, requires [ANSICON](https://github.com/adoxa/ansicon) to patch.
 ---
----@return boolean
-function warna.windows_enable_sequences()
+--- For Windows 10 before build 14393 (Anniversary update) or before Windows 10, requires [ANSICON](https://github.com/adoxa/ansicon) to patch.
+---@param force_ansicon boolean Force use ANSICON to enable SGR
+---@return boolean # Wether it successfully enable SGR
+---@return string # A short message with which method of the function is using
+function warna.windows_enable_sequences(force_ansicon)
     local ok, ffi = pcall(require, "ffi")
     if not ok and ffi then
         ok, ffi = pcall(require, "cffi")
     end
 
     if not (ok and on_windows) or (ok and ffi.os ~= "Windows") then
-        return false
+        return false, "not windows"
     end
 
     local _, ver = (cmd_output("ver") or ""):match("(%[Version (.+)%])$")
     local winver, buildver = ver:match("^(%d+%.%d+)%.(%d+)")
 
-    if ver and (tonumber(winver) >= 10 and not buildver >= "14393") or tonumber(winver) < 10 then
-        return execute_cmd((os.getenv("ANSICON") or "ansicon") .. " -p") == 0
+    if ver and (tonumber(winver) >= 10 and not buildver >= "14393") or tonumber(winver) < 10 or force_ansicon then
+        return execute_cmd((os.getenv("ANSICON") or "ansicon") .. " -p 2>1 1>NUL") == 0, "ansicon method"
     end
 
     if not ok and on_windows then
         if execute_cmd("reg query HKCU\\CONSOLE /v VirtualTerminalLevel 2>1 1>NUL") == 0 then
-            return true
+            return true, "registry method"
         end
 
-        io.write
-        return execute_cmd("reg add HKCU\\CONSOLE /f /v VirtualTerminalLevel /t REG_DWORD /d 1 2>1 1>NUL") == 0
+        io.stderr:write("This script will attempt to edit Registry to enable SGR, allow? [Yy/n...]")
+        local yn = io.read(1)
+
+        return yn:lower() == "y"
+                and execute_cmd("reg add HKCU\\CONSOLE /f /v VirtualTerminalLevel /t REG_DWORD /d 1 2>1 1>NUL") == 0
+            or false, "registry method"
     end
 
     -- stylua: ignore
@@ -91,6 +95,7 @@ function warna.windows_enable_sequences()
     local winapi = ffi.load("kernel32.dll")
 
     return winapi.SetConsoleMode(winapi.GetStdHandle(-11), 7) ~= 0
+        and winapi.SetConsoleMode(winapi.GetStdHandle(-12), 7) ~= 0, "winapi method"
 end
 
 return warna

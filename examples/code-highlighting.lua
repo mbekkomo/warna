a43b19a249692605875bace89d5c3243b539815b
komothecat <afiqquraisyzulkarnain@gmail.com>
2024-02-13 20:04:58 +0000

    chore: fix attributes in examples

--- examples/code-highlighting.lua
+++ examples/code-highlighting.lua
@@ -6,5 +6,5 @@ local warna = require("warna")
 package.path = oldpath
 
 print(warna.format[[
-%{magenta}local%{reset} str %{cyan}=%{reset} %{red}string%{white dim}.%{blue}upper%{white dim}(%{green}'hello world'%{white dim})%{reset}
+%{magenta}local%{reset} str %{cyan}=%{reset} %{red}string%{white dim}.%{reset blue}upper%{white dim}(%{reset green}'hello world'%{white dim})%{reset}
 %{blue}print%{white dim}(%{reset}str%{white dim})]])
--- examples/code-highlighting.sh
+++ examples/code-highlighting.sh
@@ -7,6 +7,6 @@ function warna.lua
 }
 
 warna.lua "\
-%{magenta}local%{reset} str %{cyan}=%{reset} %{red}string%{white dim}.%{blue}upper%{white dim}(%{green}'hello world'%{white dim})%{reset}
+%{magenta}local%{reset} str %{cyan}=%{reset} %{red}string%{white dim}.%{reset blue}upper%{white dim}(%{reset green}'hello world'%{white dim})%{reset}
 %{blue}print%{white dim}(%{reset}str%{white dim})
 "
